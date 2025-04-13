f154345b0bab8aabb398483d9fc7886ae8a4d349
Luis Fedrizze <luisfedrizze@gmail.com>
2025-04-13 16:33:18 -0300

    code cleanup

--- batch-processor.js
+++ batch-processor.js
@@ -4,16 +4,6 @@ const term = require( 'terminal-kit' ).terminal
 const parquet = require('parquetjs')
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-// 1744061915905
-
-/*
-    playerName: player.name
-    tagLine: player.tagline
-    matchIds: playerMatches.data
-    matches: [],
-    lane: player.line
-*/
-
 // PROGRAM
 const { program } = require('commander');
 
@@ -70,13 +60,9 @@ const fn = async() => {
             assists: 0,
             teamKills: 0,
             totalDamageDealtToChampions: 0,
-            // dano total da equipe aos campeões -> iterar em todos os jogadores da mesma equipe e somar o dano
             teamChampionDamage: 0,
-            // pegar totalMinionsKilled do oponente da mesma lane -> enemyTotalMinionsKilled
             enemyTotalMinionsKilled: 0,
-            // pegar goldEarned do oponente
             enemyGoldEarned: 0,
-            // pegar champExperience do oponente
             enemyChampExperience: 0,
             gameTime: 0,
             visionScore: 0,
@@ -99,12 +85,6 @@ const fn = async() => {
             const participantId = match.metadata.participants.findIndex(value => player.puuid === value) + 1
             const participantData = match.info.participants.find(value => value.participantId === participantId)
 
-            if (!participantData) {
-                console.log(player.playerName, player.puuid)
-                console.log(match.info.participants.map(v => [v.riotIdGameName, v.puuid]))
-                process.exit(0)
-            }
-            
             // FIND PARTICIPANT TEAM
             const participantTeam   = match.info.teams.find(v => v.teamId === participantData.teamId)
 
@@ -113,6 +93,10 @@ const fn = async() => {
                 value => value.teamId !== participantData.teamId &&
                 value.teamPosition === participantData.teamPosition
             )
+
+            if (!enemyParticipant) {
+                continue // skip if oponent isn't playing
+            }
             
             if (participantData.win) {
                 outData.victory += 1
@@ -127,7 +111,7 @@ const fn = async() => {
             outData.assists += participantData.assists
             outData.teamKills += participantTeam.objectives.champion.kills
             outData.totalDamageDealtToChampions += participantData.totalDamageDealtToChampions
-            outData.gameTime += match.info.gameDuration / 60 // em minutos
+            outData.gameTime += match.info.gameDuration / 60 // convert to minutes
             outData.visionScore += participantData.visionScore,
             outData.visionScorePerMinute += participantData.challenges.visionScorePerMinute
             outData.visionWardsBoughtInGame += participantData.visionWardsBoughtInGame
@@ -139,7 +123,9 @@ const fn = async() => {
             outData.firstBloodKill += participantData.firstBloodKill
             outData.firstBloodAssist += participantData.firstBloodAssist
             outData.champExperience += participantData.champExperience
-            outData.teamChampionDamage += match.info.participants.map(v => v.teamId === participantTeam.teamId ? v.totalDamageDealtToChampions : 0).reduce((p, c) => (p ?? 0) + c)
+            outData.teamChampionDamage += match.info.participants
+                .map(v => v.teamId === participantTeam.teamId ? v.totalDamageDealtToChampions : 0)
+                .reduce((p, c) => (p ?? 0) + c)
             outData.enemyTotalMinionsKilled += enemyParticipant?.totalMinionsKilled ?? 0
             outData.enemyGoldEarned += enemyParticipant?.goldEarned ?? 0
             outData.enemyChampExperience += enemyParticipant?.champExperience ?? 0
@@ -150,52 +136,7 @@ const fn = async() => {
     }
 
     term('\n\n')
-
     if (result.length === 0) return
-
-    // ENCONTRAR O VALOR MÍNIMO - o valor mais baixo entre os jogadores
-    // kda, victory, defeat
-
-    // const min = {
-    //     kills: _.minBy(result, v => v.kills).kills,
-    //     deaths: _.minBy(result, v => v.deaths).deaths,
-    //     assists: _.minBy(result, v => v.assists).assists,
-    //     kda: _.minBy(result, v => v.kda).kda,
-    //     victory: _.minBy(result, v => v.victory).victory,
-    //     defeat: _.minBy(result, v => v.defeat).defeat,
-    //     visionScore: _.minBy(result, v => v.visionScore).visionScore,
-    // }
-
-    // const max = {
-    //     kills: _.maxBy(result, v => v.kills).kills,
-    //     deaths: _.maxBy(result, v => v.deaths).deaths,
-    //     assists: _.maxBy(result, v => v.assists).assists,
-    //     kda: _.maxBy(result, v => v.kda).kda,
-    //     victory: _.maxBy(result, v => v.victory).victory,
-    //     defeat: _.maxBy(result, v => v.defeat).defeat,
-    //     visionScore: _.maxBy(result, v => v.visionScore).visionScore,
-    // }
-
-    // (metrica - min) / max - min
-    // avgs
-    // result = result.map(r => {
-    //     r.kda = (r.kills + r.deaths + r.assists) / r.gameCount
-    //     r.winRate = r.victory / (r.victory + r.defeat)
-    //     r.killRate = (r.kills + r.assists) / r.teamKills
-    //     r.avgKills = r.kills / r.gameCount
-    //     r.avgDeaths = r.deaths / r.gameCount
-    //     r.avgDamage = r.totalDamageDealtToChampions / (r.gameCount * r.gameTime)
-    //     r.avgVisionScore = r.visionScore / r.gameCount
-    //     r.avgGameVisionScore = r.visionScore / (r.gameCount * r.gameTime)
-
-    //     r.normalizedKills       = (r.kills - min.kills) / (max.kills - min.kills)
-    //     r.normalizedDeaths      = (r.deaths - min.deaths) / (max.deaths - min.deaths)
-    //     r.normalizedAssists     = (r.assists - min.assists) / (max.assists - min.assists)
-    //     r.normalizedVictory     = (r.victory - min.victory) / (max.victory - min.victory)
-    //     r.normalizedDefeat      = (r.defeat - min.defeat) / (max.defeat - min.defeat)
-    //     r.normalizedVisionScore = (r.visionScore - min.visionScore) / (max.visionScore - min.visionScore)
-    //     return r
-    // })
 
     const resultFile = fs.createWriteStream(`./results/result-${batchId}.csv`)
     csv.stringify(
